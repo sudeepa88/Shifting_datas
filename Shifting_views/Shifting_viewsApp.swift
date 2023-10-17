278aa5979c05ec041eddcd9365cf4495b54a22d8
Sudeepa Pal <85659225+sudeepa88@users.noreply.github.com>
2023-10-17 22:52:53 +0530

    Initial Commit

---
+++ Shifting_views/Shifting_viewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Shifting_viewsApp.swift
+//  Shifting_views
+//
+//  Created by sudeepa pal on 17/10/23.
+//
+
+import SwiftUI
+
+@main
+struct Shifting_viewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

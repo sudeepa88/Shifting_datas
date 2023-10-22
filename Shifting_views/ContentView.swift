3dfa38e56534ba2fd622192c9b701b09ef2e9951
Sudeepa Pal <85659225+sudeepa88@users.noreply.github.com>
2023-10-23 00:39:51 +0530

    Able to Save the views

--- Shifting_views/ContentView.swift
+++ Shifting_views/ContentView.swift
@@ -10,16 +10,21 @@ import SwiftUI
 struct ContentView: View {
   
     @State var  searchedText: String = ""
-   
+    @State var enteredTexts: [String] = []
     var body: some View {
         
         VStack{
             NavigationLink{
-                SearchView(searchedText: $searchedText)
+                SearchView(searchedText: $searchedText, enteredText:$enteredTexts)
             }label: {
                 Image(systemName: "plus")
             }
             Text(self.searchedText)
+            ForEach(enteredTexts, id: \.self){ id in
+                VStack {
+                    Text(id)
+                }
+            }//id in loop
             
         }.padding()
 }
--- Shifting_views/SearchView.swift
+++ Shifting_views/SearchView.swift
@@ -8,20 +8,31 @@
 import SwiftUI
 
 struct SearchView: View {
+    @Environment(\.presentationMode) var presentationMode
     @Binding var searchedText: String
+    @Binding var enteredText:[String]
     var body: some View {
         VStack{
             
             TextField("Search here..",text:$searchedText)
                 .keyboardType(.webSearch)
                 .disableAutocorrection(true)
+            
+            Button(action: {
+                self.enteredText.append(self.searchedText)
+                self.searchedText = ""
+                self.presentationMode.wrappedValue.dismiss()
+            }) {
+                Text("Submit")
+            }
         }
     }
 }
 
 struct SearchView_Previews: PreviewProvider {
     @State static var searchedText: String = ""
+    @State static var enteredText: [String] = []
     static var previews: some View {
-        SearchView(searchedText:$searchedText)
+        SearchView(searchedText:$searchedText,enteredText:$enteredText)
     }
 }
